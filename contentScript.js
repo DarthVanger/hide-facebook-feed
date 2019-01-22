f676725d8840a7c67a24e6491b96f8796fdeaefb
DarthVanger <darthvanger@gmail.com>
2019-01-22 18:44:31 +0200

    Add "hide feed" button to the popup

---
+++ contentScript.js
@@ -0,0 +1,4 @@
+const feedElement =  document.querySelector('div[role="feed"]');
+const storiesElement =  document.querySelector('div#fb_stories_card_root');
+feedElement.classList.add('no-feed-extension-on');
+storiesElement.classList.add('no-feed-extension-on');
--- popup.js
+++ popup.js
@@ -1,10 +1,24 @@
-let showFeedButton = document.getElementById('showFeedButton');
+const showFeedButton = document.getElementById('showFeedButton');
+const hideFeedButton = document.getElementById('hideFeedButton');
 
 showFeedButton.onclick = function(element) {
+  showFeedButton.style.display = 'none';
+  hideFeedButton.style.display = 'block';
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.executeScript(
 				tabs[0].id,
 				{file: 'showFeed.js'}
+    );
+	});
+};
+
+hideFeedButton.onclick = function(element) {
+  hideFeedButton.style.display = 'none';
+  showFeedButton.style.display = 'block';
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+				tabs[0].id,
+				{file: 'hideFeed.js'}
     );
 	});
 };
