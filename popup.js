a53e2aceb992b206fd6df1a1ab6c2718c54487f7
DarthVanger <darthvanger@gmail.com>
2019-01-22 18:44:56 +0200

    Replace tabs with spaces in popup.js

--- popup.js
+++ popup.js
@@ -4,21 +4,21 @@ const hideFeedButton = document.getElementById('hideFeedButton');
 showFeedButton.onclick = function(element) {
   showFeedButton.style.display = 'none';
   hideFeedButton.style.display = 'block';
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-				tabs[0].id,
-				{file: 'showFeed.js'}
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {file: 'showFeed.js'}
     );
-	});
+  });
 };
 
 hideFeedButton.onclick = function(element) {
   hideFeedButton.style.display = 'none';
   showFeedButton.style.display = 'block';
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.executeScript(
-				tabs[0].id,
-				{file: 'hideFeed.js'}
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {file: 'hideFeed.js'}
     );
-	});
+  });
 };
